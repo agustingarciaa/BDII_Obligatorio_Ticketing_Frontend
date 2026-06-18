6cb21f19738022b2a6c00d56f42b9850e17c63d2
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2026-06-18 18:40:46 -0300

    cambios y mejoras para partidos abm

--- frontend_ticketing/app/page.tsx
+++ frontend_ticketing/app/page.tsx
@@ -1,4 +1,5 @@
 import DashboardHero from "@/components/DashboardHero";
+import LoginModal from "@/components/modals/loginModal";
 import NavbarGeneral from "@/components/navbars/NavbarGeneral";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="wc-hero ">
       <NavbarGeneral />
       <DashboardHero />
+      <LoginModal />
     </div>
   );
 }
